46370ab309ce290add8bdd89c0ecb64bbcb3b253
sgrshah <sgrshah@gmail.com>
2013-06-29 18:56:50 -0400

    completed hw

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'multi_json'
+gem 'debugger'
 
 group :development do
   gem 'shotgun'
--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'bundler'
+require 'debugger'
 
 Bundler.require
 
@@ -16,6 +17,26 @@ module Citibike
 
     get '/' do
       erb :home
+    end
+
+    get '/form' do
+      erb :form
+    end
+
+    # post '/route' do
+    #   @params = params
+    #   erb :route
+    # end
+
+    post '/map' do
+      @start = params["start"].split(",").collect do |element| 
+        element.to_f / 1000000
+      end
+      @end = params["end"].split(",").collect do |element|
+        element.to_f / 1000000
+      end
+      # debugger
+      erb :map
     end
 
   end
